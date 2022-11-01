299a4024a4485a4c610936bbeebe60a09a3ac816
bloreincode <bblorein@gmail.com>
2022-11-01 03:17:05 +0300

    Its done
    Its finished. I added remove object from CoreData and Application its not explained in course.

--- SeyahatKitabi/FirstViewController.swift
+++ SeyahatKitabi/FirstViewController.swift
@@ -6,41 +6,135 @@
 //
 
 import UIKit
+import CoreData
 
 class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
-    
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    var selectedName = ""
+    var selectedId : UUID?
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
-        // Do any additional setup after loading the view.
+        //Table View üzerindeki + butonu oluşturuldu
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addClicked))
-        
+        //veri çekme fonksiyonumuzun çalışması için viewdidload altında çağırdık.
+        getData()
+    }
+    //Her görünüm göründüğünde çağrılması için willappear fonksiyonunda gerçekleştirdik. Eğer viewDidLoad içinde yapsaydık bir kere gerçekleşirdi.
+    override func viewWillAppear(_ animated: Bool) {
+        //Bir gözlemci ekleyip, gözlemcinin bu view controller oldugunu belirttik. Hangi işlem yapıldığında gözlemlensin  kısmını Veriler çekildiğinde yani (getData) çalıştığında olsun dedik ve notification.name diğer view controllerdaki isimle aynı olmalı dedik.
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name("newPlace"), object: nil)
+    }
+    
+   @objc func getData () {
+       let appDelegate = UIApplication.shared.delegate as! AppDelegate
+       let context = appDelegate.persistentContainer.viewContext
+       let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Travels")
+       fetchRequest.returnsObjectsAsFaults = false
+       
+       do {
+           let results = try context.fetch(fetchRequest)
+           
+           if results.count > 0 {
+               //her eklenende bir öncekini tutmayıp listeyi temizleyip sıfırdan çağırması için
+               self.nameArray.removeAll(keepingCapacity: false)
+               self.idArray.removeAll(keepingCapacity: false)
+               //results'ı dönüp her bir veriyi result olarak aldık. Daha sonra aldığım verilere if let kontrolü yaparak scope'ta tanımladığımız arrayler'e append ettik.
+               for result in results as! [NSManagedObject] {
+                   if let name = result.value(forKey: "travelName") as? String {
+                       nameArray.append(name)
+                   }
+                   if let id = result.value(forKey: "id") as? UUID {
+                       idArray.append(id)
+                   }
+                   tableView.reloadData()
+               }
+           }
+           
+       } catch {
+           print("There is an Error Occured from getData")
+       }
+       
     }
     
 
     // Table View üzerinde kaç satır görünecek
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return nameArray.count
     }
     //Table View satırlarında hangi veri görünecek
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         var content = cell.defaultContentConfiguration()
-        content.text = "Test"
+        content.text = nameArray[indexPath.row]
         cell.contentConfiguration = content
         return cell
     }
     //TopBar butonu kullanıldığında segue ile diğer VC'ye geçip pin kaydetme işlemi gerçekletirmek için segue
     @objc func addClicked () {
+        selectedName = ""
         performSegue(withIdentifier: "toDetailVC", sender: nil)
     }
     //Segue için hazırlık, Id'si şu olan segue'nin VC'si budur.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toDetailVC" {
             let destinationVC = segue.destination as! ViewController
+            destinationVC.chosenName = selectedName
+            destinationVC.chosenID = selectedId
         }
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedName = nameArray[indexPath.row]
+        selectedId = idArray[indexPath.row]
+        performSegue(withIdentifier: "toDetailVC", sender: nil)
+    }
+    
+    //uygulamadan ve CoreData üzerinden silme işlemlerini gerçekleştirdik. Kursun içerisinde yoktu kendim ekledim.
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Travels")
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                //Tekrar fetchRequest işlemleri ile Core Data üzerinden ilgili Entity'e ulaşıp verileri aldık daha sonra context.fetch işlemini gerçekleştirdik ve gelen veri var mı kontrolü yaptık. if let kontrolü ile "id" adında yeni bir değişken oluşturup gelen veride tipi UUID ve ismi id olan bir veri varsa ekranda tıklanan satırdaki id'yi veritabanından sildik ve bu satırdaki Name'i sildik. Daha sonrasında güncel verileri görmek için tableView.reloadData diyerek CoreData'da bulunan güncel verileri tekrar ekrana getirdik.
+                
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject]{
+                        if let id = result.value(forKey: "id") as? UUID {
+                            
+                            if id == idArray[indexPath.row] {
+                                //gelen sonuçlardan tıklanan satırdaki id'yi sildik
+                                context.delete(result)
+                                //tıklanan satırdaki name'i sildik
+                                nameArray.remove(at: indexPath.row)
+                                
+                                self.tableView.reloadData()
+                                
+                                do {
+                                    //tekrar coreData'ya güncel halini kaydettik.
+                                    try context.save()
+                                } catch {
+                                    print("hata var burda")
+                                }
+                                break
+                            }
+                        }
+                    }
+                }
+            } catch {
+                print("Problem occured while deleting. ")
+            }
+            
+        }
+    }
+    
 }
--- SeyahatKitabi/ViewController.swift
+++ SeyahatKitabi/ViewController.swift
@@ -14,10 +14,19 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var commentField: UITextField!
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var saveButton: UIButton!
     
     var locationManager = CLLocationManager()
     var chosenLatitude = Double()
     var chosenLongitude = Double()
+    
+    var chosenName = ""
+    var chosenID : UUID?
+    var annotationName = ""
+    var annotationComment = ""
+    var annotationLatitude = Double()
+    var annotationLongitude = Double()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,10 +43,74 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         //Lokasyona ne kadar süre tıklandıktan sonra pinleme işleminin gerçekleşmesini istediğimiz kısım
         gestureRecognizer.minimumPressDuration = 2
         mapView.isUserInteractionEnabled = true
+        
+        //Table View üzerinden seçilen kayıtlı verinin nameField bölümü, kaydedilirken doldurulmuşsa aşağıdaki adımları gerçekleştiriyoruz.
+        if chosenName != "" {
+            //App delegate'e ulaştık Core Data üzerinden veri çekme işlemleri için
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            //Context'imizi yazdık
+            let context = appDelegate.persistentContainer.viewContext
+            //fetchRequest işleminin nerden yapılacağını (CoreData üzerinde Travels olarak oluşturduğumuz Entity) ve Bize ne tür bir veri döndüreceğini belirttik.
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Travels")
+            fetchRequest.returnsObjectsAsFaults = false
+            // Bir idString yapısı tanımladık ve uuidstring olacağını söyledik
+            let idString = chosenID!.uuidString
+            //fetchRequest işleminde neye göre veriyi getireceğimizi belirttik (Yani UUID olarak oluşturduğumuz objeleri UUIDString üzerinden çekiyoruz)
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString)
+            
+            //Daha sonrasında viewContext'e ulaşmak ve fetch ile veri çekme işleminin konumunu belirtmek için ; context.fetch yapımıza Gelecek verinin nereden geleceği ve nasıl geleceğini belirttik.
+            do {
+                let results =  try context.fetch(fetchRequest)
+                //Eğer veri geliyorsa ve gelen verilerin CoreData üzerine Eklediğimiz Travels Entity'si üzerindeki karşılıkları ile girilen verilerin türlerinin eşleşmediği durum için if let yapısı ile projeyi sağlama aldık.
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let name = result.value(forKey: "travelName") as? String {
+                            annotationName = name
+                        }
+                        if let comment = result.value(forKey: "travelComment") as? String {
+                            annotationComment = comment
+                        }
+                        if let latitude = result.value(forKey: "latitude") as? Double {
+                            annotationLatitude = latitude
+                        }
+                        if let longitude = result.value(forKey: "longitude") as? Double {
+                            annotationLongitude = longitude
+                        }
+                        // Bir Annotation oluşturduk (Pinleme işareti), Kullanıcı TableView üzerinden kaydettiği Adresleri tekrar görmek istediği zaman bu pinleme işlemi haritada çıkabilsin diye.
+                        
+                        let annotation = MKPointAnnotation()
+                        //Annotation'a kullanıcının girdiği name ve commentlerin TableView'dan gidildiğinde de gösterilmesini sağladık
+                        annotation.title = annotationName
+                        annotation.subtitle = annotationComment
+                        //Veriler harita üzerine geldiğinde kaydedilen konumun da doğru gelmesi için enlem ve boylam değerlerimizi de Pinleme üzerine aktardık.
+                        let coordinate = CLLocationCoordinate2D(latitude: annotationLatitude, longitude: annotationLongitude)
+                        annotation.coordinate = coordinate
+                            //Table View'dan tıklanınca gidilen Map'e annotation'ı verdik
+                        mapView.addAnnotation(annotation)
+                        //Kullanıcının girdiği name ve comment bölümlerini Pin üzerinde gösterdik.
+                        nameField.text = annotationName
+                        commentField.text = annotationComment
+                        locationManager.stopUpdatingLocation()
+                        //Yukarıda bir coordinate oluşturduğumu için yine aynısını kullanabileceğiz bu yüzden bir span oluşturuyoruz. Ardından bu span'i ve yukarıda bulunan coordinate'i kullanarak region'dan bulunulan bölgeyi belirtiyoruz. Daha sonrasında bu bölgeyi mapView'da gösteriyoruz Bu sayede kullanıcının kaydettiği Konum tam olarak haritada gözükmüş oluyor.
+                        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                        let region = MKCoordinateRegion(center: coordinate, span: span)
+                        mapView.setRegion(region, animated: true)
+                        saveButton.isHidden = true
+                    }
+                }
+            } catch {
+                print("error")
+            }
+                    }
+        else {
+            // Add new content
+        }
     }
 //Kullanıcıdan enlem ve boylama göre konum almak için
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //Gelen veride kaydedilen bölgenin ismi yoksa kullanıcının konumunu tekrar güncellemesini istemediğimiz için böyle bir önlem aldık. Eğer konumumn ismi bulunmuyorsa kullanıcının yeni konumunu göstermeyecek.
+        if chosenName == "" {
         //lokasyon işlemleri için enlem ve boylam'a özel oluşturulmuş fonksiyonu çağırıp kullandık
         let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
         //zoom seviyesi == span
@@ -45,7 +118,56 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         //Kullanıcının bulunduğu bölgeyi bulmak için yukarıda oluşturduğumuz enlem ve boylam ve zoomlama seviyesi bilgileriyle kullanıcının anlık bulunduğu noktayı bulduk daha sonrasında bu noktayı mapView üzerinde gösterdik.
         let region = MKCoordinateRegion(center: location, span: span)
         mapView.setRegion(region, animated: true)
+        }
         
+    }
+    //Kullanıcı belli bir yeri pinledikten daha sonra bu konuma yol tarifi alabilmesi için pinleme işaretinin yanına bir buton oluşturuyoruz. Daha sonrasında butona basan kullanıcının mevcut konumunu alıp, bu kayıtlı konuma yol tarifini göstereceğiz.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        //tekrar kullanmak için bir id tanımladık
+        var reuseId = "myAnnotation"
+        //bir pinView oluşturduk, tekrar kullanılabilir olması için bunun bir MKPinAnnotationView olduğunu söyledik ve bir pinView döndürdük.
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            //Callout: Bir baloncuk içerisinde ekstra bilgi gösterilebilen alan.
+            pinView?.canShowCallout = true
+            pinView?.tintColor = UIColor.black
+            //Detay butonu oluşturduk
+            let button = UIButton(type: UIButton.ButtonType.detailDisclosure)
+            //Oluşturduğumuz butonu sağ tarafta gösterdik.
+            pinView?.rightCalloutAccessoryView = button
+            
+        } else {
+            pinView?.annotation = annotation
+        }
+        return pinView
+    }
+    //Pin'e tıklandıktan sonra çıkan information butonuna yaptırılmak istenenlerin yazıldığı fonksiyon.
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if chosenName != "" {
+            //istenilen konum tekrar oluşturuldu.
+            let requestLocation = CLLocation(latitude: annotationLatitude, longitude: annotationLongitude)
+            //mevcut konum ile gidilecek konum arasında bağlantı kurmak için gerekli hazır fonksiyon
+            CLGeocoder().reverseGeocodeLocation(requestLocation) { placemarks, error in
+                //placemarks nil değilse kontrolü yapmak için if let yapısı kurduk.
+                //placemark objesini reverse geocode location ile alıp kullanabiliyoruz.
+                if let placemark = placemarks {
+                    if placemark.count > 0 {
+                        let newPlacemark = MKPlacemark(placemark: placemark[0])
+                        //Navigasyonu açabilmek için bir tane MapItem tanımladık.
+                        let item = MKMapItem(placemark: newPlacemark)
+                        item.name = self.annotationName
+                        //Bu mapItem'ın hangi modu kullanmasını istediğimizi söyledik DirectionModeDriving (Arabayla)
+                        let launchOptions = [MKLaunchOptionsDirectionsModeKey:MKLaunchOptionsDirectionsModeDriving]
+                        item.openInMaps(launchOptions: launchOptions)
+                    }
+                }
+                
+            }
+        }
     }
     
     @objc func mapPinned(gestureRecognizer:UILongPressGestureRecognizer) {
@@ -88,6 +210,9 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         } catch {
             print("error")
         }
+        
+        NotificationCenter.default.post(name: NSNotification.Name("newPlace"), object: nil)
+        navigationController?.popViewController(animated: true)
     }
     
     
